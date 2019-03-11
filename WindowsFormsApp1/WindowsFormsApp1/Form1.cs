639e071ce4af392fee31b0b056f72190fa66cc86
yourName <you@email.address>
2019-03-11 14:05:22 +0800

    master button 1 modify

--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -30,6 +30,8 @@ namespace WindowsFormsApp1
         private void button1_Click(object sender, EventArgs e)
         {
             button1.Text = "hello";
+
+            //master
         }
     }
 }
